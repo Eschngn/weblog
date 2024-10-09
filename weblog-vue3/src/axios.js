02b6047436020ed00be512b752567afe5b4dc956
Summer Snow <ScoMorii@163.com>
2024-10-09 12:39:09 +0800

    后台首页bug修改,图床(Minio)添加子域名

--- weblog-vue3/src/axios.js
+++ weblog-vue3/src/axios.js
@@ -1,7 +1,6 @@
 import axios from "axios";
-import { getToken } from "@/composables/auth";
+import { getToken } from "@/composables/cookie"
 import { showMessage} from '@/composables/util'
-import { removeToken } from '@/composables/cookie'
 import { useUserStore } from '@/stores/user'
 
 // 创建 Axios 实例
@@ -9,6 +8,7 @@ const instance = axios.create({
     baseURL: "/api", // 你的 API 基础 URL
     timeout: 7000, // 请求超时时间
 })
+
 
 // 添加请求拦截器
 instance.interceptors.request.use(function (config) {
@@ -21,6 +21,7 @@ instance.interceptors.request.use(function (config) {
         // 添加请求头, key 为 Authorization，value 值的前缀为 'Bearer '
         config.headers['Authorization'] = 'Bearer ' + token
     }
+
     return config;
 }, function (error) {
     // 对请求错误做些什么
@@ -45,6 +46,7 @@ instance.interceptors.response.use(function (response) {
         // 刷新页面
         location.reload()
     }
+
     // 若后台有错误提示就用提示文字，默认提示为 '请求失败'
     let errorMsg = error.response.data.message || '请求失败'
     // 弹错误提示
